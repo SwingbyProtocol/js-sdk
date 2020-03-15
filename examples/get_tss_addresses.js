00a1475ea080e04893a63767f8760e8418f0f78b
SwingbyDevJ <SwingbyDevJ@swingby.network>
2020-03-15 17:15:04 +0000

    examples: add get_tss_addresses example

---
+++ examples/get_tss_addresses.js
@@ -0,0 +1,13 @@
+const sdk = require('../')
+
+const client = new sdk.NodeHttpClient({ url: "https://testnet-node.swingby.network" })
+
+client.getTssAddresses()
+    .then((addresses) => {
+        console.log("Current node TSS addresses: \n")
+        addresses.forEach((addr) => {
+            console.log(` - ${addr.currency}: ${addr.address}`)
+        })
+        console.log()
+    })
+    .catch(console.error)
