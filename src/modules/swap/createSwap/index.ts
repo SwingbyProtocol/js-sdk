433ec8cbafe5cc41a7ed12ed0c0f1457ebfce9a3
Rubén <ruben@swingby.network>
2020-11-24 17:56:05 +0100

    fix: Export

--- src/modules/swap/createSwap/index.ts
+++ src/modules/swap/createSwap/index.ts
@@ -1,5 +1,6 @@
 import { getNetwork } from '../../context';
 import { fetch } from '../../fetch';
+import { logger } from '../../logger';
 import { Mode } from '../../modes';
 import { CommonSwapParams } from '../../swap-params';
 
@@ -15,39 +16,42 @@ type Result<M extends Mode> = Pick<
   'addressIn' | 'addressOut' | 'amountIn' | 'currencyIn' | 'currencyOut' | 'nonce' | 'timestamp'
 >;
 
-export const createSwap = async <M extends Mode>({
-  context,
-  currencyOut,
-  currencyIn,
-  addressOut,
-  amountUser,
-}: Params<M>): Promise<Result<M>> => {
-  const { amountIn, nonce } = await calculateSwap({
-    context,
-    addressOut,
-    amountUser,
-    currencyIn,
-    currencyOut,
-  });
+export const createSwap = async <M extends Mode>(params: Params<M>): Promise<Result<M>> =>
+  createSwapRec({ ...params, pastExecutions: 0 });
+
+const createSwapRec = async <M extends Mode>({
+  pastExecutions,
+  ...params
+}: Params<M> & { pastExecutions: number }): Promise<Result<M>> => {
+  logger('Will execute createSwap(%O). Past executions: "%d".', params, pastExecutions);
+
+  const { amountIn, nonce } = await calculateSwap(params);
 
   type ApiResponse = Pick<
     CommonSwapParams<M>,
     'addressIn' | 'addressOut' | 'amountIn' | 'currencyIn' | 'currencyOut' | 'nonce'
   > & { timestamp: number };
 
-  const network = getNetwork({ currencyIn, currencyOut });
-  const result = await fetch<ApiResponse>(`${context.servers[network].swap}/api/v1/swaps/create`, {
-    method: 'post',
-    body: JSON.stringify({
-      address_to: addressOut,
-      amount: amountIn,
-      currency_from: currencyIn,
-      currency_to: currencyOut,
-      nonce,
-    }),
-  });
+  const network = getNetwork(params);
+  const result = await fetch<ApiResponse>(
+    `${params.context.servers[network].swap}/api/v1/swaps/create`,
+    {
+      method: 'post',
+      body: JSON.stringify({
+        address_to: params.addressOut,
+        amount: amountIn,
+        currency_from: params.currencyIn,
+        currency_to: params.currencyOut,
+        nonce,
+      }),
+    },
+  );
 
   if (!result.ok) {
+    if (/the send amount does not contain a valid proof of work/.test(result.response)) {
+      return createSwapRec({ ...params, pastExecutions: pastExecutions + 1 });
+    }
+
     throw new Error(`${result.status}: ${result.response}`);
   }
 
--- src/modules/swap/index.ts
+++ src/modules/swap/index.ts
@@ -1,3 +1,2 @@
-export { calculateSwap } from './calculateSwap';
 export { createSwap } from './createSwap';
 export { estimateAmountOut } from './estimateAmountOut';
