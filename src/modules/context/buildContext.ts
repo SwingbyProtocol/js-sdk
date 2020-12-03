0adbfc6f64caf2f6dd7def8eed48f589f6031bc7
Rubén <ruben@swingby.network>
2020-12-03 13:15:57 +0100

    fix: `buildContext` type

--- src/modules/context/buildContext.ts
+++ src/modules/context/buildContext.ts
@@ -2,7 +2,11 @@ import type { Mode } from '../modes';
 
 import type { SwingbyContext } from './SwingbyContext';
 
-export const buildContext = async ({ mode }: { mode: Mode }): Promise<SwingbyContext> => {
+export const buildContext = async <M extends Mode>({
+  mode,
+}: {
+  mode: M;
+}): Promise<SwingbyContext<M>> => {
   return {
     mode,
     servers: {
