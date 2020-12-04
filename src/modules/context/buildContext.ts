75c26e3bcc509b54b06b52cc67298bb51c45d10e
Rubén <ruben@swingby.network>
2020-12-04 17:13:33 +0100

    feat: Use seed API

--- src/modules/context/buildContext.ts
+++ src/modules/context/buildContext.ts
@@ -1,3 +1,5 @@
+import type { Bridge } from '../bridges';
+import { fetch } from '../fetch';
 import type { Mode } from '../modes';
 
 import type { SwingbyContext } from './SwingbyContext';
@@ -7,16 +9,30 @@ export const buildContext = async <M extends Mode>({
 }: {
   mode: M;
 }): Promise<SwingbyContext<M>> => {
+  const result = await fetch<
+    {
+      [k in Mode]: {
+        explorers: string[];
+        swapNodes: { [k in Bridge]: string[] };
+        indexerNodes: { [k in Bridge]: string[] };
+      };
+    }
+  >('https://seed-kappa.vercel.app/api/network');
+
+  if (!result.ok) {
+    throw new Error(`${result.status}: ${result.response}`);
+  }
+
   return {
     mode,
     servers: {
       swapNode: {
-        btc_erc: 'https://tbtc-goerli-1.swingby.network',
-        btc_bep: 'https://testnet-node.swingby.network',
+        btc_erc: result.response[mode].swapNodes.btc_erc[0],
+        btc_bep: result.response[mode].swapNodes.btc_bep[0],
       },
       indexer: {
-        btc_erc: 'https://indexer-goerli.swingby.network',
-        btc_bep: 'https://indexer-tbtc.swingby.network',
+        btc_erc: result.response[mode].indexerNodes.btc_erc[0],
+        btc_bep: result.response[mode].indexerNodes.btc_bep[0],
       },
     },
   };
