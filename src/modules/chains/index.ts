1c0fb5c734119d4c181d355ff8f8b8e2b6d1387c
Rubén <ruben@swingby.network>
2020-12-14 11:02:44 +0100

    feat: `getChainFor()` works with `sbBTC`

--- src/modules/chains/index.ts
+++ src/modules/chains/index.ts
@@ -3,12 +3,13 @@ import type { Coin } from '../coins';
 const CHAINS = ['bitcoin', 'ethereum', 'binance'] as const;
 export type Chain = typeof CHAINS[number];
 
-export const getChainFor = ({ coin }: { coin: Coin }): Chain => {
+export const getChainFor = ({ coin }: { coin: Coin | 'sbBTC' }): Chain => {
   switch (coin) {
     case 'BTCB':
       return 'binance';
     case 'BTC':
       return 'bitcoin';
+    case 'sbBTC':
     case 'WBTC':
       return 'ethereum';
   }
