9983dc1f43a96b8ed8a83436a7c463b47f29a8b4
tarrio <73276750+tarrio@users.noreply.github.com>
2020-12-27 15:23:57 +0100

    feat: Randomly pick a node when calling `buildContext()` (#15)

--- src/modules/context/buildContext.ts
+++ src/modules/context/buildContext.ts
@@ -4,6 +4,8 @@ import type { SkybridgeMode } from '../modes';
 
 import type { SkybridgeContext } from './SkybridgeContext';
 
+const randomInt = (min: number, max: number) => Math.round(Math.random() * (max - min)) + min;
+
 export const buildContext = async <M extends SkybridgeMode>({
   mode,
 }: {
@@ -27,12 +29,24 @@ export const buildContext = async <M extends SkybridgeMode>({
     mode,
     servers: {
       swapNode: {
-        btc_erc: result.response[mode].swapNodes.btc_erc[0],
-        btc_bep: result.response[mode].swapNodes.btc_bep[0],
+        btc_erc:
+          result.response[mode].swapNodes.btc_erc[
+            randomInt(0, result.response[mode].swapNodes.btc_erc.length - 1)
+          ],
+        btc_bep:
+          result.response[mode].swapNodes.btc_bep[
+            randomInt(0, result.response[mode].swapNodes.btc_bep.length - 1)
+          ],
       },
       indexer: {
-        btc_erc: result.response[mode].indexerNodes.btc_erc[0],
-        btc_bep: result.response[mode].indexerNodes.btc_bep[0],
+        btc_erc:
+          result.response[mode].indexerNodes.btc_erc[
+            randomInt(0, result.response[mode].indexerNodes.btc_erc.length - 1)
+          ],
+        btc_bep:
+          result.response[mode].indexerNodes.btc_bep[
+            randomInt(0, result.response[mode].indexerNodes.btc_bep.length - 1)
+          ],
       },
     },
   };
