7e03f0dfbd7d56dbbce329b694a768de0ccdc77e
Rubén <ruben@swingby.network>
2021-01-12 12:04:33 +0100

    feat: Support for the affiliate program

--- src/modules/context/SkybridgeContext.ts
+++ src/modules/context/SkybridgeContext.ts
@@ -3,6 +3,7 @@ import type { SkybridgeBridge } from '../bridges';
 
 export type SkybridgeContext<M extends SkybridgeMode = SkybridgeMode> = {
   readonly mode: M;
+  readonly affiliateApi?: string;
   readonly servers: {
     readonly swapNode: { readonly [k in SkybridgeBridge]: string };
     readonly indexer: { readonly [k in SkybridgeBridge]: string };
--- src/modules/swap/createSwap/index.ts
+++ src/modules/swap/createSwap/index.ts
@@ -1,6 +1,42 @@
 import type { SkybridgeMode } from '../../modes';
 import { create, CreateParams, CreateResult } from '../../generic-create';
+import { logger } from '../../logger';
+import { fetch } from '../../fetch';
 
 export const createSwap = async <M extends SkybridgeMode>(
-  params: Omit<CreateParams<'swap', M>, 'resource'>,
-): Promise<CreateResult<'swap', M>> => create({ ...params, resource: 'swap' });
+  params: Omit<CreateParams<'swap', M>, 'resource'> & { affiliateCode?: string | null },
+): Promise<CreateResult<'swap', M>> => {
+  const result = await create({ ...params, resource: 'swap' });
+
+  (async () => {
+    try {
+      if (!params.context.affiliateApi) {
+        logger('No affiliate program API present in context.');
+        return;
+      }
+
+      if (!params.affiliateCode) {
+        logger('No affiliate code has been passed.');
+        return;
+      }
+
+      const linkResult = await fetch<{ affiliateCode: string }>(
+        `${params.context.affiliateApi}/api/swaps/link`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ affiliateCode: params.affiliateCode, swapHash: result.hash }),
+        },
+      );
+
+      if (!linkResult.ok) {
+        throw new Error(`Affiliate code linking failed: ${linkResult.response}`);
+      }
+
+      logger('Swap linked to affiliate code: %O', linkResult.response);
+    } catch (e) {
+      logger('Failed calling the affiliate program API: %s', e.message);
+    }
+  })();
+
+  return result;
+};
