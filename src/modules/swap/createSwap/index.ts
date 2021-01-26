e089ad67c5beb2b2e8f4e186349188fb74d16c4e
Rubén <ruben@swingby.network>
2021-01-26 16:26:18 +0100

    fix: Call `/swaps/link` with GET instead of POST

--- src/modules/swap/createSwap/index.ts
+++ src/modules/swap/createSwap/index.ts
@@ -21,11 +21,7 @@ export const createSwap = async <M extends SkybridgeMode>(
       }
 
       const linkResult = await fetch<{ affiliateCode: string }>(
-        `${params.context.affiliateApi}/api/${params.context.mode}/swaps/link`,
-        {
-          method: 'POST',
-          body: JSON.stringify({ affiliateCode: params.affiliateCode, swapHash: result.hash }),
-        },
+        `${params.context.affiliateApi}/api/${params.context.mode}/swaps/link?affiliateCode=${params.affiliateCode}&swapHash=${result.hash}`,
       );
 
       if (!linkResult.ok) {
