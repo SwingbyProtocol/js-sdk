a0b5fd2a15f784d5e85bf929664d4f2a997c5320
Roy <rswkure@gmail.com>
2022-01-14 20:59:25 +0800

    fix: use node-api to fetch the peers data instead of network-api (#32)

---
+++ src/modules/endpoints/index.ts
@@ -0,0 +1,10 @@
+export const FIXED_NODE_ENDPOINT = {
+  btc_erc: {
+    production: ['https://taitan-0083.zoo.farm'],
+    test: ['https://tbtc-ropsten-node-1.swingby.network'],
+  },
+  btc_bep20: {
+    production: ['https://ra-cailum.zoo.farm'],
+    test: ['https://tbtc-bsc-2.swingby.network', 'https://tbtc-bsc-3.swingby.network'],
+  },
+};
