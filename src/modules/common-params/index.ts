29ea36be99dd426a2af4a8e10b3886713943756b
Roy <rswkure@gmail.com>
2022-02-03 14:49:47 +0800

    feat: added terms message

--- src/modules/common-params/index.ts
+++ src/modules/common-params/index.ts
@@ -94,3 +94,6 @@ export type SkybridgeQuery = {
   txIdIn?: string;
   txIdOut?: string;
 };
+
+export const SkybridgeTermsMessage =
+  'I am not the person or entities who reside in, are citizens of, are incorporated in, or have a registered office in the United States of America or any prohibited localities, as defined in the Terms of Use. (https://docs.swingby.network/terms.pdf)I will not in the future access this site or use Swingby dApps while located within the United States any prohibited localities, as defined in the Terms of Use.I am not using, and will not in the future use, a VPN to mask my physical location from a restricted territory.I am legally permitted to access this site and use Swingby dApps under the laws of the jurisdiction in which I reside and where I am located.I understand the risks related to using Swingby Network protocols.';
